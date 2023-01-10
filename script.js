6a0de1640aab815e81939b55ae514e7610c3d480
Parrotastic <13218687+Parrotastic@users.noreply.github.com>
2023-01-09 21:30:45 -0800

    Added game logic

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -29,8 +31,13 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //Add dice to current score
     currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+
     current0El.textContent = currentScore; //TODO: CHANGE LATER
   } else {
-    //Swwitch to next player
+     activePlayer = activePlayer === 0 ? 1 : 0;
+    }
+    //Switch to next player
   }
 });
